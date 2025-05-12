2728eab8d68f137769e6461cfea213ee77933132
Gabriele Okuyama <58577350+AkemiOkuyama@users.noreply.github.com>
2025-05-12 19:50:57 -0300

    att

--- Códigos em Aula/Aula 6/script.js
+++ Códigos em Aula/Aula 6/script.js
@@ -2,3 +2,8 @@ function alterarTexto() {
     document.getElementById("titulo").innerText = "Você clicou!";
     document.getElementById("titulo").classList.add("destaque");
     }
+
+
+function Retornar(){
+    document.getElementById("titulo").innerText = "Olhe para o botão.";
+}
